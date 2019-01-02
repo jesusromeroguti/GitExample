d46a1a9513fe03862e3078fd4f0720c6d09f49ef
Jesus Romero Gutierrez <jrg.jesusromero@gmail.com>
2019-01-02 13:04:12 +0100

    Ejemplo de añadir branches

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Proyecto de ejemplo para probar las funcionalidades de git")
+        let today = NSDate()
+        print(today)
     }
 
 
